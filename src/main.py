887ec94db7e8f48e260951d100ea7c73175a919b
Noah Huetter <noahhuetter@gmail.com>
2020-09-18 23:40:17 +0200

    MVC framework with threads set up

--- src/main.py
+++ src/main.py
@@ -1,9 +1,53 @@
-from PyQt5.QtWidgets import QApplication, QLabel
 
-app = QApplication([])
+import logging
+import threading
+import time
 
-label = QLabel('Hello World!')
 
-label.show()
+from model import Model
+from view import View
 
-app.exec_()
+if __name__ == "__main__":
+
+	# setup logger
+  format = "%(asctime)s: %(message)s"
+  logging.basicConfig(format=format, level=logging.INFO,
+                      datefmt="%H:%M:%S")
+
+  # create model thread
+  model = Model("model")
+  modelThd = threading.Thread(target=model.modelThread)
+
+  # create main view thread
+  view = View("view")
+  viewThd = threading.Thread(target=view.viewThread)
+
+  # connect model and view
+  model.setView(view)
+  view.setModel(model)
+
+  # start threads
+  logging.info("Main    : starting model thread")
+  modelThd.start()
+  logging.info("Main    : starting view thread")
+  viewThd.start()
+  
+
+  # x.join()
+  time.sleep(1)
+  view.terminate()
+  model.terminate()
+  viewThd.join()
+  modelThd.join()
+  logging.info("Main    : exiting")
+
+
+# from PyQt5.QtWidgets import QApplication, QLabel
+
+# app = QApplication([])
+
+# label = QLabel('Hello World!')
+
+# label.show()
+
+# app.exec_()
---
+++ src/view.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# @Author: Noah Huetter
+# @Date:   2020-09-18 23:22:24
+# @Last Modified by:   Noah Huetter
+# @Last Modified time: 2020-09-18 23:39:36
+
+
+import logging
+import time
+
+class View(object):
+  """docstring for Model"""
+  def __init__(self, name):
+    self.name = name
+    self.exit = False
+    self.model = None
+
+  def viewThread(self):
+    logging.info("Thread %s: starting", self.name)
+    
+    while not self.exit:
+      # VIEW CODE COMES HERE!!!!!
+      pass
+
+    logging.info("Thread %s: finishing", self.name)
+
+
+  def terminate(self):
+    self.exit = True
+
+  def setModel(self, model):
+    self.model = model
+
+
+if __name__ == "__main__":
+  print("view main()")
